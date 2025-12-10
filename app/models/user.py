2810f34dc89d3dc0faad0332321fa02be1ff9183
waiyanhein96 <waiyanhein1314@gmail.com>
2025-12-10 19:30:05 +0700

    Add files via upload

--- app/models/user.py
+++ app/models/user.py
@@ -18,7 +18,7 @@ class DockerComposeFile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String(255), nullable=False)
     system_type = db.Column(db.String(50), nullable=False)
-    source = db.Column(db.String(50), nullable=False)  # 'local' or 'gitee'
+    source = db.Column(db.String(50), nullable=False)  # 'local' or 'github'
     file_path = db.Column(db.String(500), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
